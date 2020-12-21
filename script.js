95c21b3b1d355476865d22ccb4737722578c4558
MattiasSimonsson <Mattias.simonsson1@gmail.com>
2020-12-21 20:57:43 +0100

    delete unnecessary code

--- script.js
+++ script.js
@@ -13,59 +13,9 @@ let email = document.getElementById('email');
 
 
 
-showContactBtn.addEventListener('click', toggleDisplayContacts);
-
-/*
-let newTr = document.createElement("tr");
-let tdName = document.createElement("td");
-let tdAge = document.createElement("td");
-let tdPhone = document.createElement("td");
-let tdEmail = document.createElement("td");
-
-// submitContact.addEventListener('click', createContact);
-
-function createContact() {
-let newTr = document.createElement("tr");
-let tdName = document.createElement("td");
-let tdAge = document.createElement("td");
-let tdPhone = document.createElement("td");
-let tdEmail = document.createElement("td");
-
-tdName.innerHTML = name.value;
-tdAge.innerHTML = age.value;
-tdPhone.innerHTML = phone.value;
-tdEmail.innerHTML = email.value;
-
-document.getElementById('table').appendChild(newTr);
-document.getElementById('table').appendChild(tdName);
-document.getElementById('table').appendChild(tdAge);
-document.getElementById('table').appendChild(tdPhone);
-document.getElementById('table').appendChild(tdEmail);
-} */
-
 function submit(event) {
   addNewContact();
-   /* let contactsArray = [];
-
-  let newContact = `
-  <th>Name</th>
-  <td>${form.name.value}</td> 
-  <th>Age</th>
-  <td>${form.age.value}</td>
-  <th>Phone</th>
-  <td>${form.phone.value}</td>
-  <th>Email</th>
-  <td>${form.email.value}</td>`; 
-  let getInfo = table.innerHTML =  `<th>Name</th>
-  <td class="info">${form.name.value}</td> 
-  <th>Age</th>
-  <td class="info">${form.age.value}</td>
-  <th>Phone</th>
-  <td class="info">${form.phone.value}</td>
-  <th>Email</th>
-  <td class="info">${form.email.value}</td>`;
-  contactsArray.push(getInfo);*/
-
+  
   event.preventDefault();   
 }
 
@@ -93,7 +43,9 @@ function toggleDisplayContacts() {
 }
 
 
+// EventListeners
 form.addEventListener('submit', submit);
+showContactBtn.addEventListener('click', toggleDisplayContacts);
 
 
 
